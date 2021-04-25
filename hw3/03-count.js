e033751c7bb332b5ee642d16faf090bbf2b6f4bc
sirikoduru24 <81694764+sirikoduru24@users.noreply.github.com>
2021-04-24 21:44:31 -0700

    count js

--- hw3/03-count.js
+++ hw3/03-count.js
@@ -1,3 +1,14 @@
 // Enter your code here
-
 console.log('Enter your code here');
+var input = document.getElementById('userinput');
+var output = document.getElementById('intro');
+function highlight() {
+    let x = '\\b'+input.value+'\\b';
+    let regex = new RegExp(x,'ig');
+    let res = output.innerText.replace(regex, (matched) => {
+        return `<span style="background-color:yellow;">${matched}</span>`;
+    });
+    output.innerHTML = res;
+}
+input.addEventListener('keyup',highlight, false);
+input.addEventListener('keydown',highlight, false);
