cc51b87d6228a4cc1d85b052717b2e3d1c6c72d1
sirikoduru24 <81694764+sirikoduru24@users.noreply.github.com>
2021-04-24 22:57:14 -0700

    background.js

--- hw3/05-background.js
+++ hw3/05-background.js
@@ -16,12 +16,10 @@ btn.onclick = () => {
         stpBtn.value = 'Stop';
         input.disabled = true;
         x = setInterval(changeBgColor, input.value * 1000);
-        console.log('Set interval'+x);
     }
 }
 function changeBgColor() {
     if(btn.value == 'Stop') {
-        console.log('in change');
         var o = Math.round, r = Math.random, s = 255;
         var color = 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
         document.body.style.backgroundColor = color;
