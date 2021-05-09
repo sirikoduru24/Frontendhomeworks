0070f71248894e231be63934d19910477f30671d
sirikoduru24 <81694764+sirikoduru24@users.noreply.github.com>
2021-05-08 20:06:41 -0700

    Create Home.js

---
+++ hw4/03-react/src/Home.js
@@ -0,0 +1,12 @@
+export default function Home() {
+ 
+    return (
+      <div className="m-2 text-center">
+        <h1 class = "text-success"> Home </h1>
+        <div>
+          <h2 class = "text-info">Welcome!</h2>
+        </div>
+      </div>
+    );
+  }
+  
